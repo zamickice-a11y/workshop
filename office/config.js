7a6657754b60d32cae96992a3babc47015e6ee5a
zamickice-a11y <zamickice@gmail.com>
2026-05-29 00:53:51 +0930

    Update config.js

--- office/config.js
+++ office/config.js
@@ -5,7 +5,7 @@
 // =====================================================================
 window.SIAM_CONFIG = {
   // Your Supabase project URL, e.g. "https://qftomcqjxfvfeukaomyc.supabase.co/rest/v1/"
-  SUPABASE_URL: "https://qftomcqjxfvfeukaomyc.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://qftomcqjxfvfeukaomyc.supabase.co",
 
   // Your Supabase "anon" / "publishable" public key (safe for browser)
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdG9tY3FqeGZ2ZmV1a2FvbXljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5NzM1ODcsImV4cCI6MjA5NTU0OTU4N30.LdGdWONhMGvVQ9omuomlDg45d8BIr_8z9QYbnrWGhS8",
